51933eb526d0c2c9e36f582e47fb3d97e0a5bd63
dnguyen85 <danh@dnguyen.io>
2016-09-04 22:52:18 -0400

    Updated vim cscope to use quickfix list, added alias for `make all` Changed ranger config to launch google-chrome instead of firefox for html Added Xilinx env variables

--- system/env.zsh
+++ system/env.zsh
@@ -6,3 +6,6 @@ export LPASS_AGENT_TIMEOUT=21600
 
 # Matplotlib
 export MPLCONFIGDIR=~/.matplotlib
+
+# For Xilinx product use
+export LM_LICENSE_FILE=2100@license1.ece.drexel.edu:2100@license2.ece.drexel.edu:2100@license3.ece.drexel.edu:1717@license1.ece.drexel.edu:1717@license2.ece.drexel.edu:1717@license3.ece.drexel.edu
