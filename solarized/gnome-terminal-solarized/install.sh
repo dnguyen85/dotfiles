d8c62c1f006222341b65a78df46056881bb6b448
dnguyen85 <danh@dnguyen.io>
2016-09-13 13:18:53 -0400

    Update gnome-terminal-colors-solarized install script

--- solarized/gnome-terminal-solarized/install.sh
+++ solarized/gnome-terminal-solarized/install.sh
@@ -8,6 +8,7 @@ if [[ ! -d $INSTALL_DIR ]]; then
     mkdir -p $INSTALL_DIR
     git clone https://github.com/Anthony25/gnome-terminal-colors-solarized $INSTALL_DIR
     cd $INSTALL_DIR
-    ./install.sh
+    git checkout ceab3f6
+    ./install.sh --scheme dark --profile Default 
 fi
 
