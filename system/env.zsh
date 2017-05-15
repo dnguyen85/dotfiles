7000b7926c32059dda957fca00c82b4c47efa647
Danh Nguyen <danh@dnguyen.io>
2017-05-15 15:50:15 -0400

    Update aliases and env variables

--- system/env.zsh
+++ system/env.zsh
@@ -7,5 +7,9 @@ export LPASS_AGENT_TIMEOUT=21600
 # Matplotlib
 export MPLCONFIGDIR=~/.matplotlib
 
+# SystemC
+export SYSTEMC=~/.local
+export LD_LIBRARY_PATH=~/.local/lib:$LD_LIBRARY_PATH
+
 # For Xilinx product use
 export LM_LICENSE_FILE=2100@license1.ece.drexel.edu:2100@license2.ece.drexel.edu:2100@license3.ece.drexel.edu:1717@license1.ece.drexel.edu:1717@license2.ece.drexel.edu:1717@license3.ece.drexel.edu
--- system/path.zsh
+++ system/path.zsh
@@ -3,6 +3,6 @@ then
     export PATH="/usr/local/opt/coreutils/libexec/gnubin:$DOTFILES/bin:$PATH"
     export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 else
-    export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
+    export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.local/bin:$DOTFILES/bin:$PATH"
     export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 fi
