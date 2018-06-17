292469dd8fa7e7ad234271567077943bd6768344
Danh Nguyen <danh@dnguyen.io>
2018-06-17 19:49:24 -0400

    Update i3 and tmux options to work with latest version of ubuntu 16.04

--- system/env.zsh
+++ system/env.zsh
@@ -12,4 +12,4 @@ export SYSTEMC=~/.local
 export LD_LIBRARY_PATH=~/.local/lib:$LD_LIBRARY_PATH
 
 # For Xilinx product use
-export LM_LICENSE_FILE=2100@license1.ece.drexel.edu:2100@license2.ece.drexel.edu:2100@license3.ece.drexel.edu:1717@license1.ece.drexel.edu:1717@license2.ece.drexel.edu:1717@license3.ece.drexel.edu
+#  export LM_LICENSE_FILE=2100@license1.ece.drexel.edu:2100@license2.ece.drexel.edu:2100@license3.ece.drexel.edu:1717@license1.ece.drexel.edu:1717@license2.ece.drexel.edu:1717@license3.ece.drexel.edu
