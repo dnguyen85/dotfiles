1d07186916811a4909bd50002fdb3895e1f1eb1e
Danh Nguyen <danh@dnguyen.io>
2020-04-02 23:22:17 -0400

    Setup neovim using dotfiles

--- vim/install.sh
+++ vim/install.sh
@@ -4,3 +4,9 @@ if [[ ! -d ~/.vim ]];
 then
     git clone --recursive https://github.com/dnguyen85/vim ~/.vim
 fi
+
+if [[ ! -d ~/.config/nvim ]];
+then
+    mkdir -p ~/.config/nvim
+    ln -s ~/.dotfiles/vim/nvimrc_setup ~/.config/nvim/init.vim
+fi
--- vim/vim.zsh
+++ vim/vim.zsh
@@ -1,9 +1,2 @@
 # Default editor
 export EDITOR=vim
-
-# Stardict and sdcv
-if [[ -f "${HOME}"/.vim/bundle/vim-stardict/bindings/zsh/stardict.zsh ]]; then
-    source "${HOME}"/.vim/bundle/vim-stardict/bindings/zsh/stardict.zsh
-fi
-alias whatis="stardict"
-alias whatvim="vstardict"
