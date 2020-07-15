3ff8c74735305b96f23ec4c43d103de6e51f7c3e
Danh Nguyen <danh@dnguyen.io>
2020-07-14 20:32:21 -0700

    General config update for ranger and vim within grid

--- python/python.zsh
+++ python/python.zsh
@@ -1,3 +1,4 @@
 export WORKON_HOME=~/python_envs
 export PYTHONPATH=~/.dotfiles/python:~/.software/komodo-python-dbgp/pythonlib
-source /usr/local/bin/virtualenvwrapper_lazy.sh
+
+# extra configs set in .localrc
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -25,6 +25,11 @@ alias kh="khard"
 alias v="vdirsyncer sync"
 alias os="odrive status"
 alias or="orefresh"
-alias vim="nvim"
+#  alias vim="nvim"
 alias htop="TERM=screen-256color htop"
 alias tig="TERM=screen-256color tig"
+alias sles12sub='bsub -q priority -Is -R "select[sles12 && mem>4000] rusage[mem=4000] span[hosts=1]"'
+alias tmuxsub='bsub -Is -q interactive -W43200 -n16 -R "select[sles12 && type==LINUX64 && mem>64000] rusage[mem=64000] span[hosts=1]" mux'
+alias tmuxsub8='bsub -Is -q interactive -W43200 -n8 -R "select[sles12 && type==LINUX64 && mem>32000] rusage[mem=32000] span[hosts=1]" mux'
+alias isub='bsub -Is -q interactive -R "select[sles12 && mem>16000] rusage[mem=16000] span[hosts=1]"'
+alias boringdiff='svn diff --diff-cmd=/usr/bin/diff'
