faf9e3fbdcc46a2b807b34aaf857e1d8dd14d776
Danh Nguyen <danh@dnguyen.io>
2021-05-11 23:16:13 -0400

    Update from macOS

--- vim/install.sh
+++ vim/install.sh
@@ -10,4 +10,5 @@ then
     echo "Setting up nvim init file\n"
     mkdir -p ~/.config/nvim
     ln -s ~/.dotfiles/vim/nvimrc_setup ~/.config/nvim/init.vim
+    ln -s ~/.dotfiles/vim/ginit.vim ~/.config/nvim/ginit.vim
 fi
