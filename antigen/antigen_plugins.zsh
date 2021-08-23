1c36c3a8e7b7360faf3b2dc007f4a4fba30a589a
Danh Nguyen <danh@dnguyen.io>
2021-08-23 15:33:08 -0700

    Removed antigen. Update zsh and vim

--- antigen/antigen_plugins.zsh
+++
@@ -1,7 +0,0 @@
-# List of other antigen zsh plugins (no oh-my-zsh)
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-completions src
-antigen bundle anntzer/zsh-pandoc-completion
-
-# Apply
-antigen apply
