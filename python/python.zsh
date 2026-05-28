955d31fb2b6e5412f1b7f97eb972882581552e3d
Danh Nguyen <danhhn@amazon.com>
2026-05-28 14:10:41 +0000

    Remove pyenv setup; use mise instead
    mise replaces pyenv for Python version management; running both creates
    duplicate shims. Drop the unguarded eval calls that errored on hosts
    without pyenv installed.

--- python/python.zsh
+++ python/python.zsh
@@ -1,16 +1,8 @@
 export WORKON_HOME=~/python_envs
 export PYTHONPATH=~/.dotfiles/python:~/.software/komodo-python-dbgp/pythonlib:$PYTHONPATH
 export PYTHONBREAKPOINT=ipdb.set_trace
-#  export PATH=~/python_envs/3.8.3/bin:$PATH
 
-# Pyenv
-
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
-# Load pyenv-virtualenv automatically 
-eval "$(pyenv virtualenv-init -)"
+# Python version management is handled by mise (see .localrc)
 
 # extra configs set in .localrc
 
